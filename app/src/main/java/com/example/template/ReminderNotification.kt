ec0ca281068e5fd9192263280a90b1ac383dbef5
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-10-16 17:49:12 +0300

    Handling Backstack

--- app/src/main/java/com/example/template/ReminderNotification.kt
+++ app/src/main/java/com/example/template/ReminderNotification.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.TaskStackBuilder
 
 object ReminderNotification {
     const val REMINDER_CHANNEL="reminders"
@@ -16,6 +17,8 @@ object ReminderNotification {
                 .setType("text/plain")
                 .putExtra(Intent.EXTRA_TEXT,message),"SHARE"),PendingIntent.FLAG_UPDATE_CURRENT)
         val intent=Intent(Intent(context,Second::class.java))
+        //It ensures that once a user launches the pending intent from the notification,a backStack is maintained
+        val pendingIntent=TaskStackBuilder.create(context).addNextIntentWithParentStack(intent).getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT)
         intent.putExtra("EXTRAS","Am Deep Linked")
         val builder=NotificationCompat.Builder(context, REMINDER_CHANNEL)
             //Set Defaults for the notification ie..Light,Sound,Vibration
@@ -23,7 +26,7 @@ object ReminderNotification {
             .setSmallIcon(R.drawable.ic_release)
             .setContentTitle(titleText)
             .setContentText(message)
-                .setContentIntent(PendingIntent.getActivity(context,0,intent,PendingIntent.FLAG_UPDATE_CURRENT))
+                .setContentIntent(pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setTicker(titleText)
             .setAutoCancel(true)
