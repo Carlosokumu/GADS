1de7a0046ac0651b3e92e9aafab39c876d667675
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-10-16 16:29:11 +0300

    set Up A notification

--- app/src/main/java/com/example/template/MainActivity.kt
+++ app/src/main/java/com/example/template/MainActivity.kt
@@ -1,15 +1,64 @@
 package com.example.template
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.template.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private  lateinit var binding: ActivityMainBinding
+   // private lateinit var menuItem: ActionMenuItemView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.appbar.toolbar.inflateMenu(R.menu.menu)
+        setSupportActionBar(binding.appbar.toolbar)
+        //binding.appbar.toolbar.inflateMenu(R.menu.main_menu)
+       // menuItem= binding.appbar.toolbar.findViewById(R.id.notification) as ActionMenuItemView
+        createNotificationChannel()
     }
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.app_name)
+            val descriptionText = getString(R.string.app_name)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            val channel = NotificationChannel(ReminderNotification.REMINDER_CHANNEL, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.notification ->{
+                Toast.makeText(this,"Hre  is",Toast.LENGTH_SHORT).show()
+                ReminderNotification.notify(this,"Her","Him")
+            }
+        }
+        return true
+    }
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+
+
+
 }
