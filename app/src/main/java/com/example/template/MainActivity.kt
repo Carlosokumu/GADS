509c380dd738f3f1691af379505f8bf7887658f7
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-10-16 08:34:00 +0300

    Set up notification button

--- app/src/main/java/com/example/template/MainActivity.kt
+++ app/src/main/java/com/example/template/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.template
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.template.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.appbar.toolbar.inflateMenu(R.menu.menu)
     }
 }
