358cbe5e3bf581d401812d4f35a05151dc7cd3e2
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-10-19 07:31:28 +0300

    SignUp build up

--- app/src/main/java/com/example/template/Login.kt
+++ app/src/main/java/com/example/template/Login.kt
@@ -2,10 +2,34 @@ package com.example.template
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.template.databinding.ActivityLoginBinding
+import com.example.template.databinding.ActivitySecondBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class Login : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+
     }
+    private fun signUp(){
+        auth.createUserWithEmailAndPassword(binding.email.text.toString(),binding.password.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    val user: FirebaseUser? = auth.currentUser
+                    Toast.makeText(this,"signed up", Toast.LENGTH_SHORT).show()
+
+                } else {
+                    Toast.makeText(this,task.exception.toString(),Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
