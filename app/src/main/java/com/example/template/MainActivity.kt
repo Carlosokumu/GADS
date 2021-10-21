005dbba66a5de15b5538099c521e29826a3b15c7
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-10-21 09:26:12 +0300

    Adding update functionality

--- app/src/main/java/com/example/template/MainActivity.kt
+++ app/src/main/java/com/example/template/MainActivity.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toast
 import com.example.template.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -16,6 +17,8 @@ import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     private  lateinit var binding: ActivityMainBinding
+    private  lateinit var updateName: String
+    private  lateinit var updatePhone: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
